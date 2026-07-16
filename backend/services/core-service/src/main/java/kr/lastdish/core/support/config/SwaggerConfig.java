15e2bf877a88a3bd932275c7e27737b932055dac
minjung <alswjd0240@naver.com>
2026-07-16 15:19:40 +0900

    Docs: Swagger 설wagger 설정 추가

---
+++ backend/services/core-service/src/main/java/kr/lastdish/core/support/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package kr.lastdish.core.support.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI coreOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Core API")
+                        .version("v1"))
+                .servers(List.of(
+                        new Server().url("/core")
+                ));
+//              jwt 도입 시 수정 (Authorize 설정)
+//                .addSecurityItem(
+//                        new SecurityRequirement().addList(securitySchemeName)
+//                )
+//                .components(new Components()
+//                        .addSecuritySchemes(
+//                                securitySchemeName,
+//                                new SecurityScheme()
+//                                        .name(securitySchemeName)
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")
+//                        )
+//                );
+    }
+
+}
