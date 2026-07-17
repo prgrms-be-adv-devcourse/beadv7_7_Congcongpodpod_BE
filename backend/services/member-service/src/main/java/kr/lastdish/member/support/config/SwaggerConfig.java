b9288b3c32b370c3e1542d13e793fc48eccc4ec6
minjung <alswjd0240@naver.com>
2026-07-17 17:04:06 +0900

    Fix: spotless 반영

--- backend/services/member-service/src/main/java/kr/lastdish/member/support/config/SwaggerConfig.java
+++ backend/services/member-service/src/main/java/kr/lastdish/member/support/config/SwaggerConfig.java
@@ -3,24 +3,18 @@ package kr.lastdish.member.support.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI memberOpenAPI() {
+  @Bean
+  public OpenAPI memberOpenAPI() {
 
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Member API")
-                        .version("v1"))
-                .servers(List.of(
-                        new Server().url("/")
-                ));
-    }
-
+    return new OpenAPI()
+        .info(new Info().title("Member API").version("v1"))
+        .servers(List.of(new Server().url("/")));
+  }
 }
