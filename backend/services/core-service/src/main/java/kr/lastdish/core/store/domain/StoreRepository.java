16056fe83545cf42a0a7092993ecd60f7b4cc45c
minjung <alswjd0240@naver.com>
2026-07-19 23:02:19 +0900

    Feature: ㄴStore Repository 구í현, DIP 적용

---
+++ backend/services/core-service/src/main/java/kr/lastdish/core/store/domain/StoreRepository.java
@@ -0,0 +1,12 @@
+package kr.lastdish.core.store.domain;
+
+public interface StoreRepository {
+
+    Store save(Store store);
+
+    boolean existsByMemberId(Long memberId);
+
+    boolean existsByBusinessNumber(
+            String businessNumber
+    );
+}
