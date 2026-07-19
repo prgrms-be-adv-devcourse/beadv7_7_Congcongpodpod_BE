0b64667d7fdeb4aae43a2b615cc0a5e38cc7a7b1
minjung <alswjd0240@naver.com>
2026-07-19 23:04:29 +0900

    Feature: 매ì 매장 등록 구현, me현, memeber 역할 변경 요청 제외

---
+++ backend/services/core-service/src/main/java/kr/lastdish/core/store/presentation/StoreController.java
@@ -0,0 +1,33 @@
+package kr.lastdish.core.store.presentation;
+
+import jakarta.validation.Valid;
+import kr.lastdish.core.store.application.StoreService;
+import kr.lastdish.core.store.application.dto.StoreResult;
+import kr.lastdish.core.store.presentation.dto.StoreCreateRequest;
+import kr.lastdish.core.store.presentation.dto.StoreResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/stores")
+@RequiredArgsConstructor
+public class StoreController {
+
+    private final StoreService storeService;
+
+    @PostMapping
+    public ResponseEntity<StoreResponse> registerStore(
+            @RequestHeader("X-Member-Id") Long memberId,
+            @Valid @RequestBody StoreCreateRequest request
+    ) {
+        StoreResult result = storeService.register(
+                request.toCommand(memberId)
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(StoreResponse.from(result));
+    }
+}
