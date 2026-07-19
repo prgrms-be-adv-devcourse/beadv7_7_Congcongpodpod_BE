6117b1ea184109fdb950d77c7e278db93563cb65
minjung <alswjd0240@naver.com>
2026-07-19 23:02:19 +0900

    Feature: ㄴStore Repository 구í현, DIP 적용

---
+++ backend/services/core-service/src/main/java/kr/lastdish/core/store/infrastructure/StoreJpaRepository.java
@@ -0,0 +1,14 @@
+package kr.lastdish.core.store.infrastructure;
+
+import kr.lastdish.core.store.domain.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreJpaRepository
+        extends JpaRepository<Store, Long> {
+
+    boolean existsByMemberId(Long memberId);
+
+    boolean existsByBusinessNumber(
+            String businessNumber
+    );
+}
