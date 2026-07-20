9909eeb865d87ee7ea4c19049001a5c28b8c4f98
minjung <alswjd0240@naver.com>
2026-07-20 09:18:47 +0900

    Feature: Spotless 반영

--- backend/services/core-service/src/main/java/kr/lastdish/core/store/presentation/dto/StoreCreateRequest.java
+++ backend/services/core-service/src/main/java/kr/lastdish/core/store/presentation/dto/StoreCreateRequest.java
@@ -3,57 +3,34 @@ package kr.lastdish.core.store.presentation.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import kr.lastdish.core.store.application.dto.RegisterStoreCommand;
-
 import java.math.BigDecimal;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.List;
+import kr.lastdish.core.store.application.dto.RegisterStoreCommand;
 
 public record StoreCreateRequest(
-        @NotBlank
-        String storeName,
+    @NotBlank String storeName,
+    @NotBlank String businessNumber,
+    @NotBlank String storeAddress,
+    @NotBlank String storePhone,
+    @NotNull @JsonFormat(pattern = "HH:mm") LocalTime openTime,
+    @NotNull @JsonFormat(pattern = "HH:mm") LocalTime closeTime,
+    @NotNull BigDecimal latitude,
+    @NotNull BigDecimal longitude,
+    List<DayOfWeek> holidays) {
 
-        @NotBlank
-        String businessNumber,
-
-        @NotBlank
-        String storeAddress,
-
-        @NotBlank
-        String storePhone,
-
-        @NotNull
-        @JsonFormat(pattern = "HH:mm")
-        LocalTime openTime,
-
-        @NotNull
-        @JsonFormat(pattern = "HH:mm")
-        LocalTime closeTime,
-
-        @NotNull
-        BigDecimal latitude,
-
-        @NotNull
-        BigDecimal longitude,
-
-        List<DayOfWeek> holidays
-) {
-
-    public RegisterStoreCommand toCommand(
-            Long memberId
-    ) {
-        return new RegisterStoreCommand(
-                memberId,
-                storeName,
-                businessNumber,
-                storeAddress,
-                storePhone,
-                openTime,
-                closeTime,
-                latitude,
-                longitude,
-                holidays == null ? List.of() : holidays
-        );
-    }
+  public RegisterStoreCommand toCommand(Long memberId) {
+    return new RegisterStoreCommand(
+        memberId,
+        storeName,
+        businessNumber,
+        storeAddress,
+        storePhone,
+        openTime,
+        closeTime,
+        latitude,
+        longitude,
+        holidays == null ? List.of() : holidays);
+  }
 }
