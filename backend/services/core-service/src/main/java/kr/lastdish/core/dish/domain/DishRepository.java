5e0f1b59f56c96f84b237e7a76d421c5eca6dc23
aru.lee <aru.lee@lloydk.co.kr>
2026-07-20 15:16:53 +0900

    fix:Optional 파사드

--- backend/services/core-service/src/main/java/kr/lastdish/core/dish/domain/DishRepository.java
+++ backend/services/core-service/src/main/java/kr/lastdish/core/dish/domain/DishRepository.java
@@ -1,11 +1,9 @@
 package kr.lastdish.core.dish.domain;
 
-import java.util.Optional;
-
 public interface DishRepository {
   Dish save(Dish dish);
 
-  Optional<Dish> findById(Long dishId);
+  Dish findById(Long dishId);
 
   Dish findByIdAndIsDeletedFalse(Long dishId);
 }
