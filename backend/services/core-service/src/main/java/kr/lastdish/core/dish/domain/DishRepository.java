6f01e516c3f68df82da85ba25e1e7a8868ba08ce
aru.lee <aru.lee@lloydk.co.kr>
2026-07-20 16:43:11 +0900

    feat(Cart): 장바구니 담기/조회/삭제 구현

--- backend/services/core-service/src/main/java/kr/lastdish/core/dish/domain/DishRepository.java
+++ backend/services/core-service/src/main/java/kr/lastdish/core/dish/domain/DishRepository.java
@@ -1,9 +1,13 @@
 package kr.lastdish.core.dish.domain;
 
+import java.util.Optional;
+
 public interface DishRepository {
   Dish save(Dish dish);
 
   Dish findById(Long dishId);
 
   Dish findByIdAndIsDeletedFalse(Long dishId);
+
+  Optional<Dish> findAvailableById(Long dishId);
 }
