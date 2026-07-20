69ddf488c6f9a84bf22caef289b5521c22b4d9ab
nayoung16 <nykim1016@sookmyung.ac.kr>
2026-07-20 12:14:34 +0900

    feat: 상품 등록, 수정 시 할인율 검증 추가

--- backend/services/core-service/src/main/java/kr/lastdish/core/dish/application/DishService.java
+++ backend/services/core-service/src/main/java/kr/lastdish/core/dish/application/DishService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 @Service
 @RequiredArgsConstructor
 public class DishService {
@@ -16,6 +19,10 @@ public class DishService {
 
   @Transactional
   public DishResponse createDish(DishCreateRequest request) {
+
+    // 할인율 검증
+    validateDiscountRate(request.dishPrice(), request.discountPrice());
+
     Dish dish =
         Dish.create(
             request.storeId(),
@@ -34,6 +41,9 @@ public class DishService {
 
   @Transactional
   public DishResponse updateDish(Long dishId, DIshUpdateRequest request) {
+    // 할인율 검증
+    validateDiscountRate(request.dishPrice(), request.discountPrice());
+
     Dish dish = getDish(dishId);
     dish.update(
         request.dishName(),
@@ -45,6 +55,16 @@ public class DishService {
         request.dishPrice(),
         request.discountPrice());
     return DishResponse.from(dish);
+  }
+
+  private void validateDiscountRate(BigDecimal dishPrice, BigDecimal discountPrice) {
+    BigDecimal discountRate = dishPrice
+            .subtract(discountPrice)
+            .divide(dishPrice, 4, RoundingMode.HALF_UP);
+
+    if (discountRate.compareTo(BigDecimal.valueOf(0.3)) < 0) {
+      throw new IllegalArgumentException("할인율은 30% 이상이어야 합니다.");
+    }
   }
 
   @Transactional
