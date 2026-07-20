faef3d388936dc72a650c3456631c53dfac0c4cd
oxxultus <oxxultus@gmail.com>
2026-07-20 11:45:32 +0900

    Style: Gateway import 순서 정리

--- backend/services/gateway-service/src/main/java/kr/lastdish/gateway/security/GatewaySecurityConfig.java
+++ backend/services/gateway-service/src/main/java/kr/lastdish/gateway/security/GatewaySecurityConfig.java
@@ -1,12 +1,12 @@
 package kr.lastdish.gateway.security;
 
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
 
 /**
  * Gateway 라우팅 전에 실행되는 WebFlux Security 필터 체인을 구성한다.
