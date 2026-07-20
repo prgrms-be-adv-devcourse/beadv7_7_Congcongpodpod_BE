bfd3f94e9dd494d44925cde2cebb750fb953ccfb
nayoung16 <nykim1016@sookmyung.ac.kr>
2026-07-20 10:14:46 +0900

    feat: Dish 수정, 삭제, 조회 API 개발

--- backend/services/core-service/src/main/java/kr/lastdish/core/dish/application/DishService.java
+++ backend/services/core-service/src/main/java/kr/lastdish/core/dish/application/DishService.java
@@ -2,8 +2,9 @@ package kr.lastdish.core.dish.application;
 
 import kr.lastdish.core.dish.domain.Dish;
 import kr.lastdish.core.dish.domain.DishRepository;
+import kr.lastdish.core.dish.presentation.dto.DIshUpdateRequest;
 import kr.lastdish.core.dish.presentation.dto.DishCreateRequest;
-import kr.lastdish.core.dish.presentation.dto.DishCreateResponse;
+import kr.lastdish.core.dish.presentation.dto.DishResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,7 +15,7 @@ public class DishService {
   private final DishRepository dishRepository;
 
   @Transactional
-  public DishCreateResponse createDish(DishCreateRequest request) {
+  public DishResponse createDish(DishCreateRequest request) {
     Dish dish =
         Dish.create(
             request.storeId(),
@@ -28,7 +29,37 @@ public class DishService {
             request.discountPrice());
 
     Dish savedDish = dishRepository.save(dish);
+    return DishResponse.from(savedDish);
+  }
 
-    return DishCreateResponse.from(savedDish);
+  @Transactional
+  public DishResponse updateDish(Long dishId, DIshUpdateRequest request) {
+    Dish dish = getDish(dishId);
+    dish.update(
+            request.dishName(),
+            request.registeredAt(),
+            request.description(),
+            request.category(),
+            request.thumbnailUrl(),
+            request.stockQuantity(),
+            request.dishPrice(),
+            request.discountPrice()
+    );
+    return DishResponse.from(dish);
+  }
+
+  @Transactional
+  public void deleteDish(Long dishId) {
+      Dish dish = getDish(dishId);
+      dish.delete();
+  }
+
+  public DishResponse getEachDish(Long dishId) {
+    Dish dish = getDish(dishId);
+    return DishResponse.from(dish);
+  }
+
+  private Dish getDish(Long dishId) {
+    return dishRepository.findById(dishId);
   }
 }
