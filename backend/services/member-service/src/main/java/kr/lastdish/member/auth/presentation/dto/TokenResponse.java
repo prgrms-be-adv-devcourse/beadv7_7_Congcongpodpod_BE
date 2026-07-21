37a5c872a8f06da6c14626625e8f8ecb6b0916b7
jiwow-wow <jibbang@mju.ac.kr>
2026-07-21 11:45:22 +0900

    feat: member-service 로그인 및 인증 관련 구현

---
+++ backend/services/member-service/src/main/java/kr/lastdish/member/auth/presentation/dto/TokenResponse.java
@@ -0,0 +1,3 @@
+package kr.lastdish.member.auth.presentation.dto;
+
+public record TokenResponse(String accessToken, String refreshToken) {}
---
+++ backend/services/member-service/src/main/java/kr/lastdish/member/member/domain/RefreshTokenRepository.java
@@ -0,0 +1,5 @@
+package kr.lastdish.member.member.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {}
