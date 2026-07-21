6fe2c45aff5c5545ed982d6c4775ab56436086fc
nayoung16 <nykim1016@sookmyung.ac.kr>
2026-07-21 19:44:25 +0900

    feat: OrderCreateRequest 검증 추가

--- backend/services/core-service/src/main/java/kr/lastdish/core/order/presentation/dto/OrderCreateRequest.java
+++ backend/services/core-service/src/main/java/kr/lastdish/core/order/presentation/dto/OrderCreateRequest.java
@@ -1,15 +1,20 @@
 package kr.lastdish.core.order.presentation.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 import java.time.LocalTime;
 
 public record OrderCreateRequest(
-    Long storeId,
-    Long dishId,
-    // String memberName, // 추후 이름 추가
-    String phone,
-    String dishName,
-    Long quantity,
-    BigDecimal unitPrice,
-    LocalTime pickupStartAt,
-    LocalTime pickupEndAt) {}
+        @NotNull Long storeId,
+        @NotNull Long dishId,
+        // String memberName, // 추후 이름 추가
+        @NotBlank String phone,
+        @NotBlank String dishName,
+        @NotNull @Positive Long quantity,
+        @NotNull @Positive BigDecimal unitPrice,
+        @NotNull LocalTime pickupStartAt,
+        @NotNull LocalTime pickupEndAt) {}
