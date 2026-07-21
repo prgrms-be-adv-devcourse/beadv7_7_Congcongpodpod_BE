99b15e02465c2cfc6bed401e7f8f3febc8357f11
jiwow-wow <jibbang@mju.ac.kr>
2026-07-21 15:55:26 +0900

    fix: 회원가입 통합 테스트 및 JWT 설정 빈 등록 최종 완료

--- backend/services/member-service/src/main/java/kr/lastdish/member/auth/presentation/AuthController.java
+++ backend/services/member-service/src/main/java/kr/lastdish/member/auth/presentation/AuthController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/members")
+@RequestMapping("/api/v1/auth") // 변경됨
 @RequiredArgsConstructor
 public class AuthController {
 
