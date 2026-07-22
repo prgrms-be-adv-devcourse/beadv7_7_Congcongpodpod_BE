23e814f5c3f4f6b4377c229eb772610d9e621392
eunji1107 <dksro117@naver.com>
2026-07-22 10:29:58 +0900

    style: Spotless 적용

--- backend/services/core-service/src/main/java/kr/lastdish/core/payment/application/DepositFacade.java
+++ backend/services/core-service/src/main/java/kr/lastdish/core/payment/application/DepositFacade.java
@@ -1,4 +1,20 @@
 package kr.lastdish.core.payment.application;
 
+import java.math.BigDecimal;
+import kr.lastdish.core.payment.application.dto.DepositTransactionResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DepositFacade {
+  private final DepositService depositService;
+
+  public DepositTransactionResult use(Long memberId, Long orderId, BigDecimal amount) {
+    return depositService.use(memberId, orderId, amount);
+  }
+
+  public DepositTransactionResult refund(Long memberId, Long orderId, BigDecimal amount) {
+    return depositService.refund(memberId, orderId, amount);
+  }
 }
