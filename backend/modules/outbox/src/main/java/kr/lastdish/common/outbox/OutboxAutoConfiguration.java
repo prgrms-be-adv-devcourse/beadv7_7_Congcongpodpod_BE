75dcbce859d18b5ce06b333d822b74d052cc862d
oxxultus <a38027928@gmail.com>
2026-07-25 01:01:47 +0900

    Refactor: Outbox 이벤트 선점 의존성 역전
    - 이벤트 선점 계약을 Domain Repository로 분리
    - EntityManager 기반 구현을 Infrastructure Adapter로 구성
    - Application의 Infrastructure 직접 의존을 제거

--- backend/modules/outbox/src/main/java/kr/lastdish/common/outbox/OutboxAutoConfiguration.java
+++ backend/modules/outbox/src/main/java/kr/lastdish/common/outbox/OutboxAutoConfiguration.java
@@ -1,11 +1,7 @@
 package kr.lastdish.common.outbox;
 
-import kr.lastdish.common.outbox.application.OutboxClaimService;
-import kr.lastdish.common.outbox.application.OutboxEventProcessor;
-import kr.lastdish.common.outbox.application.OutboxEventWriter;
-import kr.lastdish.common.outbox.application.OutboxFailureRecorder;
-import kr.lastdish.common.outbox.application.OutboxScheduler;
-import kr.lastdish.common.outbox.infrastructure.OutboxClaimRepository;
+import kr.lastdish.common.outbox.application.*;
+import kr.lastdish.common.outbox.infrastructure.OutboxClaimRepositoryAdapter;
 import kr.lastdish.common.outbox.infrastructure.OutboxEventSerializer;
 import kr.lastdish.common.outbox.infrastructure.OutboxRepositoryAdapter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
@@ -15,17 +11,20 @@ import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @AutoConfiguration(
-    before = {HibernateJpaAutoConfiguration.class, DataJpaRepositoriesAutoConfiguration.class})
+    before = {
+        HibernateJpaAutoConfiguration.class,
+        DataJpaRepositoriesAutoConfiguration.class
+    })
 @EnableScheduling
 @Import({
-  OutboxAutoConfigurationPackagesRegistrar.class,
-  OutboxClaimService.class,
-  OutboxEventProcessor.class,
-  OutboxEventWriter.class,
-  OutboxFailureRecorder.class,
-  OutboxScheduler.class,
-  OutboxClaimRepository.class,
-  OutboxEventSerializer.class,
-  OutboxRepositoryAdapter.class
+    OutboxAutoConfigurationPackagesRegistrar.class,
+    OutboxClaimService.class,
+    OutboxEventProcessor.class,
+    OutboxEventWriter.class,
+    OutboxFailureRecorder.class,
+    OutboxScheduler.class,
+    OutboxClaimRepositoryAdapter.class,
+    OutboxEventSerializer.class,
+    OutboxRepositoryAdapter.class
 })
 public class OutboxAutoConfiguration {}
