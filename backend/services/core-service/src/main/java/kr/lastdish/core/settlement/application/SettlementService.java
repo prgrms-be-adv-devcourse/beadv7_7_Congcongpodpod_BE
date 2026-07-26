65376053201da151362e389dbaeedf4f8fd507c2
minjung <alswjd0240@naver.com>
2026-07-26 18:20:13 +0900

    Fix: 매장 계좌 누락 수정

--- backend/services/core-service/src/main/java/kr/lastdish/core/settlement/application/SettlementService.java
+++ backend/services/core-service/src/main/java/kr/lastdish/core/settlement/application/SettlementService.java
@@ -69,7 +69,7 @@ public class SettlementService {
     List<OrderSettlementAmount> calculatedOrders = calculateOrders(unsettledOrders);
 
     // 정산 생성
-    Settlement settlement = createSettlement(storeId, settlementMonth, period, calculatedOrders);
+    Settlement settlement = createSettlement(storeId, settlementMonth, period, calculatedOrders, account);
 
     Settlement savedSettlement = settlementRepository.save(settlement);
 
