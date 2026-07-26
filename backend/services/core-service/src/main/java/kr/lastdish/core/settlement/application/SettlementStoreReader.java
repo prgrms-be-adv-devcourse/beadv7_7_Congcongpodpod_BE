d3696f366a6a1dbe6d47c1c174506841bce33e2d
minjung <alswjd0240@naver.com>
2026-07-26 17:13:15 +0900

    Featuer: ì re° 계좌 추가

--- backend/services/core-service/src/main/java/kr/lastdish/core/settlement/application/SettlementStoreReader.java
+++ backend/services/core-service/src/main/java/kr/lastdish/core/settlement/application/SettlementStoreReader.java
@@ -1,7 +1,11 @@
 package kr.lastdish.core.settlement.application;
 
+import kr.lastdish.core.settlement.application.dto.SettlementAccountData;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface SettlementStoreReader {
   List<Long> readSettlementTargetStoreIds();
+  Optional<SettlementAccountData> readAccountByStoreId(Long storeId);
 }
